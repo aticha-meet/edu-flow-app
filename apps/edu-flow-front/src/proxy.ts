ce0a101be8bccc9953c8315f691d66b1d23d39d8
Jaosou <89335399+Jaosou@users.noreply.github.com>
2026-07-08 21:18:33 +0700

    Change proxy exam to public

--- apps/edu-flow-front/src/proxy.ts
+++ apps/edu-flow-front/src/proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // Public paths ที่ไม่ต้องการ authentication
-const PUBLIC_PATHS = ['/login', '/callback', '/api/auth'];
+const PUBLIC_PATHS = ['/login', '/callback', '/api/auth','/exam'];
 
 export async function proxy(request: NextRequest) {
     const { pathname } = request.nextUrl;
